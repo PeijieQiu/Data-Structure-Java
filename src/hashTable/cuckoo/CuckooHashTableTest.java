106231414099053457f736f0984307c1e7985792
PeijieQiu <ricardo.chiu@outlook.com>
2019-05-19 11:59:45 +0800

    Initial Commit

--- src/hashTable/cuckoo/CuckooHashTableTest.java
+++ src/hashTable/cuckoo/CuckooHashTableTest.java
@@ -1,13 +1,13 @@
 package hashTable.cuckoo;
 
 public class CuckooHashTableTest {
-    public static void main(String[] args){
-        StringHashFamily stringHashFamily=new StringHashFamily(5);
+    public static void main(String[] args) {
+        StringHashFamily stringHashFamily = new StringHashFamily(5);
         //定义布谷鸟散列
         CuckooHashTable<String> cuckooHashTable = new CuckooHashTable<>(stringHashFamily);
-        String[] strs = {"abc","aba","abcc","abca"};
+        String[] strs = {"abc", "aba", "abcc", "abca"};
         //插入
-        for (int i = 0; i < strs.length; i ++){
+        for (int i = 0; i < strs.length; i++) {
             cuckooHashTable.insert(strs[i]);
         }
         //打印表
